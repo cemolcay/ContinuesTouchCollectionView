7f7436180b3c2f4a83e9130a587be3e78b6fae79
cemolcay <ccemolcay@gmail.com>
2018-08-21 13:59:45 +0300

    Initial Commit

---
+++ ContinuesTouchCollectionView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ContinuesTouchCollectionView
+//
+//  Created by Cem Olcay on 21.08.2018.
+//  Copyright © 2018 cemolcay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
